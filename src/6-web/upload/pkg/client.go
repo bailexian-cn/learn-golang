6ab0271ee39ae53b90c5d9e222a77315f6de36e3
薛宇 <345526579@qq.com>
2023-01-21 13:35:47 +0800

    init

--- src/6-web/upload/pkg/client.go
+++ src/6-web/upload/pkg/client.go
@@ -3,15 +3,13 @@ package pkg
 import (
 	"io"
 	"io/ioutil"
-	"mime/multipart"
 	"net/http"
 )
 
 func Upload(uploadUrl string, reader io.Reader) error {
 	client := &http.Client{}
-	mr := multipart.NewReader(reader, "chart")
 
-	req, err := http.NewRequest(http.MethodPut, uploadUrl, mr)
+	req, err := http.NewRequest(http.MethodPut, uploadUrl, reader)
 	if err != nil {
 		return err
 	}
