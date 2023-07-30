bb69ae07efefc3ad3735c7144da13cd413f15da7
薛宇 <345526579@qq.com>
2023-07-30 11:17:00 +0800

    init

--- utils/net/tcp/ipv4_test.go
+++ utils/net/tcp/ipv4_test.go
@@ -31,3 +31,49 @@ func TestElectNoUsedIpFromSubnet(t *testing.T) {
 		t.Errorf("except 192.168.0.0, but %s", noUsedIp)
 	}
 }
+
+func TestCheckIpv4Format(t *testing.T) {
+	validIps := []string{
+		"192.168.0.1",
+		"1.1.1.1",
+	}
+	invalidIps := []string{
+		"1.1.1.1.",
+		"a.1.3",
+	}
+	for _, ip := range validIps {
+		e1 := CheckIpv4Format(ip)
+		if e1 != nil {
+			t.Error(e1)
+		}
+	}
+	for _, ip := range invalidIps {
+		e2 := CheckIpv4Format(ip)
+		if e2 == nil {
+			t.Errorf("except err, but no err for %s", ip)
+		}
+	}
+}
+
+func TestCheckIpv4CidrFormat(t *testing.T) {
+	validCidrs := []string{
+		"192.168.0.0/16",
+		"1.1.1.1/24",
+	}
+	invalidCidrs := []string{
+		"1.1.1.3",
+		"3.3.3.3/44",
+	}
+	for _, cidr := range validCidrs {
+		e1 := CheckIpv4CidrFormat(cidr)
+		if e1 != nil {
+			t.Errorf("execpt no err, but err %s for %s", e1, cidr)
+		}
+	}
+	for _, cidr := range invalidCidrs {
+		e2 := CheckIpv4CidrFormat(cidr)
+		if e2 == nil {
+			t.Errorf("except err, but no err for %s", cidr)
+		}
+	}
+}
